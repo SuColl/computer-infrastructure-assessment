832616b4dd2f0278b1729385cc898dafbb857d94
SuColl <susanbot@gmail.com>
2025-12-21 02:00:09 +0000

    Improve notebook narrative

--- faang.py
+++ faang.py
@@ -1,5 +1,11 @@
 #! /usr/bin/env python
 
+# Module for handling date and time data, 
+import datetime as dt
+# Module for interfacing with the operating system, 
+import os 
+# Module for regular expressions, used for string matching 
+import re
 # import NumPy to manipulate numerical arrays
 import numpy as np
 # import Python Data Analysis Library
@@ -7,24 +13,21 @@ import pandas as pd
 # import Matplotlib.PyPlot for plotting
 import matplotlib.pyplot as plt
 # import yfinance for financial data for these exercises
-# https://github.com/ranaroussi/yfinance
 import yfinance as yf
-# Library for handling date and time data, https://docs.python.org/3/library/datetime.html
-import datetime as dt
-# Library for interfacing with the operating system, https://docs.python.org/3/library/os.html
-import os 
-# Library for regular expressions, used for string matching
-import re
+
 
 # Function to download hourly finance data for specified stock trackers
 def get_finance_data_hourly(ticker_list, period='5d'):
-    """Download Yahoo Finance data for supplied list of trackers, default period 5 days."""
+    """Download Yahoo Finance data for supplied list of trackers, 
+    default period 5 days.
+    """
 
     # Use the yfinance.download function with multiple stock tickers
-    downloaded_dataframe = yf.download(ticker_list, period=period, interval='1h', auto_adjust=True)
+    downloaded_dataframe = yf.download(
+        ticker_list, period=period, interval='1h', auto_adjust=True)
 
     # Look at the first 5 rows of the resulting DataFrame.
-    print(f"Downloaded DataFrame Head: \n {downloaded_dataframe.head(5)}")
+    print(f"Downloaded DataFrame Shape: \n {downloaded_dataframe.shape}")
     return(downloaded_dataframe)
 
 
@@ -37,11 +40,13 @@ def csv_filename_from_now():
     # Show.
     print(f"Current date and time are {now}.")
 
-    # Use datetime.datetime.strftime to create the output filename as a string, 
-    # in the requested format.  
+    # Use datetime.datetime.strftime to create the output filename 
+    # as a string, formatted as per this assignment's instructions.  
     # Handy cheat sheet for strftime format specifiers available at
     # https://strftime.org/ 
     csv_filename = now.strftime("%Y%m%d-%H%M%S.csv")
+
+    # Print result for the user.
     print(f"Generated CSV filename is {csv_filename}.")
 
     # Return the 
@@ -51,6 +56,7 @@ def csv_filename_from_now():
 # Function to check if a directory exists
 def check_directory(directory_to_check):
     """Check if a specified directory exists, if not, create the directory."""
+
     if not os.path.isdir(directory_to_check):
         os.makedirs(directory_to_check)
         print(f"Created directory at {directory_to_check}")
@@ -60,6 +66,8 @@ def check_directory(directory_to_check):
 
 # Function to download all hourly finance data, and export to CSV file
 def get_data():
+    """Download hourly stock data, export to CSV file named with datetime."""
+
     # Download hourly stock data
     df_downloaded = get_finance_data_hourly(tickers)
 
@@ -76,7 +84,9 @@ def get_data():
 
 # Function to get the latest valid data file, using filename
 def get_latest_valid_file(directory):
-    """Return the file in a directory with the latest timestamp in the filename."""
+    """Return the file in a directory with the latest timestamp in the 
+    filename.
+    """
 
     # List the files in the data_read directory
     files_in_directory = os.listdir(directory)
@@ -91,8 +101,8 @@ def get_latest_valid_file(directory):
     # Create empty list to hold matching filenames
     files_match_pattern= []
 
-    # Iterate through the files in the data_read directory and get the ones with 
-    # filenames matching the pattern 
+    # Iterate through the files in the data_read directory and get the 
+    # ones with filenames matching the pattern 
     for file in files_in_directory:
         if re.match(search_pattern, file):
             # If a filename matches the regex string, add it to the new list
@@ -110,16 +120,22 @@ def get_latest_valid_file(directory):
 
 
 def read_stock_data_from_csv(file):
-    """Read stock data from a CSV file that was written from a yfinance download."""
+    """Read stock data from a CSV file that was written from a 
+    yfinance download.
+    """
 
     # Reading DataFrame from CSV file using pandas.read_csv()
     # (Ref: https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html).
     # Parameters:
-    # header=[0,1] - The first two rows of the CSV file contain MultiIndex header information,
-    # index_col=0 - I want column 0, the dates, to be the index of the resulting DataFrame,
-    # parse_dates=[0] - I want the index values to be parsed as dates (i.e. as datetime objects.)
+    # header=[0,1] - The first two rows of the CSV file contain 
+    # MultiIndex header information,
+    # index_col=0 - I want column 0, the dates, to be the index of the 
+    # resulting DataFrame,
+    # parse_dates=[0] - I want the index values to be parsed as dates 
+    # (i.e. as datetime objects.)
     # (adapted from https://stackoverflow.com/a/37453925)
-    dataframe_read = pd.read_csv(file, header=[0,1], index_col=0, parse_dates=[0])
+    dataframe_read = pd.read_csv(file, header=[0,1], 
+                                index_col=0, parse_dates=[0])
 
     # Print info to let the user know what has been done
     print(f"Read in: {file}")
@@ -130,7 +146,10 @@ def read_stock_data_from_csv(file):
 
 # Function to extract the datetime string from a supplied filename
 def remove_filename_extension(file):
-    """Removes the extension from a filename. Assumes maximum of 2 period characters in filename."""
+    """Removes the extension from a filename. 
+    Assumes maximum of 2 period characters in filename.
+    """
+
     # Split the string at the period character and return the first part.
     string = file.rsplit( ".", 2 )[ 0 ]
     return string
@@ -147,25 +166,27 @@ def make_png_from_finance_data(dataframe, datetime_string, output_directory):
     fig.subplots_adjust(hspace=0)
 
     # plot Close prices and Volume on separate subplots. 
-    # Close prices are plotted without using the datetimes in the data index, i.e. 
-    # the periods are plotted all in sequence. 
-    # Volume is a stacked bar plot. 
+    # Close prices are plotted without using the datetimes in the data index, 
+    # i.e. the periods are plotted equally spaced on the x-axis. 
+    # Volume data is plotted as a stacked bar plot. 
     dataframe["Close"].plot(ax=top, use_index=False)
     dataframe["Volume"].plot(ax=bot, kind='bar', stacked=True, legend=None)
 
     # set plot title including the timestamp in the name of the read CSV file
-    plt.suptitle("Hourly close prices and volume for each stock, " + datetime_string, y=0.93)
+    plt.suptitle("Hourly close prices and volume for each stock, " 
+                + datetime_string, y=0.93)
+    
     # set labels for x-axis and both y-axes
     bot.set_xlabel("Trading Periods (UTC)")
     bot.set_ylabel("Volume")
     top.set_ylabel("Hourly Close Price ($)")
 
     # Set fixed major and minor x-tick locations. 
-    # Major tick is the start of each trading day at 14:30 UTC. 
-    # Minor tick is every other hourly datapoint/  
+    # Date ticks / major ticks are the start of each trading day at 14:30 UTC. 
     ticks_date = dataframe.index.indexer_at_time('14:30')
-    ticks_time = np.arange(dataframe.index.size) # step in hours
     top.set_xticks(ticks_date)
+    # Time ticks / minor ticks are every hourly datapoint/ 
+    ticks_time = np.arange(dataframe.index.size) # step in hours
     top.set_xticks(ticks_time, minor=True)
 
     # add vertical gridlines at the start of each day
@@ -181,19 +202,21 @@ def make_png_from_finance_data(dataframe, datetime_string, output_directory):
                     for maj_tick in dataframe.index[ticks_date]]
     labels_time = [min_tick.strftime('%H:%M')
                     for min_tick in dataframe.index[ticks_time]]
-    top.set_xticklabels(labels_date)
-    top.set_xticklabels(labels_time, minor=True)
-    top.figure.autofmt_xdate(rotation=0, ha='center', which='both')
+    bot.set_xticklabels(labels_date)
+    bot.set_xticklabels(labels_time, minor=True)
+    bot.figure.autofmt_xdate(rotation=0, ha='center', which='both')
 
     # Save plot to file
-    plt.savefig(output_directory + datetime_string + ".png")
+    plt.savefig(output_directory + datetime_string + ".png", dpi=300)
 
     # Print message for user
     print(f"Plot created at {output_directory + datetime_string}.png")
 
 
 def plot_data():
-    """Function to open latest data file, plot the Close prices, and save as PNG"""
+    """Function to open latest data file, plot the Close prices, 
+    and save as PNG
+    """
 
     # Check if the data directory exists
     check_directory(data_read_dir)
